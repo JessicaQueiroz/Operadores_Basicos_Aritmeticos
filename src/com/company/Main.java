999a1e47542abf18b80089817b66570f5dc45c4d
JessicaQueiroz <jehqueiroz01@gmail.com>
2020-09-30 19:55:04 -0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int numero1 = 10;
+	    int numero2 = 5;
+	    int soma = numero1 + numero2;
+	    int subtracao = numero1 - numero2;
+	    int multiplicacao = numero1 * numero2;
+	    int divisao = numero1 / numero2;
+	    int modulo = numero1 % numero2;
+
+	    System.out.printf("Soma: %d, Subtração: %d, Multiplcação: %d, Divisão: %d, Módulo: %d", soma, subtracao, multiplicacao, divisao, modulo);
+
+    }
+}
